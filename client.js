f5893a6c19087274d9aa00359994b9d8c6489b80
Danny Jiang <dannyjiang11@outlook.com>
2022-04-14 08:08:42 +0000

    added messages to client

--- client.js
+++ client.js
@@ -8,13 +8,14 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
   conn.on('connect', () => {
+    console.log("Successfully connected to game server"); // welcome greeting on connection
     conn.write("Name: DAN"); // writes data to the server
     //conn.write("Move: up")
   });
+  conn.on("data", (data) => {
+    console.log("Server says: ", data); // message from client
+  });
   return conn;
 };
-
-console.log("Connecting ...");
-connect();
 
 module.exports = connect
