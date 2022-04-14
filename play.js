8d3236218f4c14beedac49cb424a7f5b1bea3706
Danny Jiang <dannyjiang11@outlook.com>
2022-04-14 08:04:03 +0000

    moved setupInput function into input.js

--- play.js
+++ play.js
@@ -1,22 +1,9 @@
 const net = require("net");
 
-const connect = require('./client')
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
-const setupInput = function (conn) {
-  connection = conn;
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+console.log("Connecting ...");
+connect();
 
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }  
-};
-
-
-module.exports = setupInput;
+setupInput();
